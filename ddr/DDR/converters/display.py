f96dc68d5868c9e07cca0ab420ba55390e0d8293
geoffrey jost <geoffrey@jostwebwerks.com>
2016-08-19 16:50:24 -0700

    converters.display.render now uses Jinja2

--- ddr/DDR/converters/display.py
+++ ddr/DDR/converters/display.py
@@ -1,19 +1,17 @@
 import os
 
+from jinja2 import Template
+
 
 # module function ------------------------------------------------------
 
-def render_simple(template, data):
-    """Render a template using str.format().
+def render(template, data):
+    """Render a Jinja2 template.
+    
+    @param template: str Jinja2-formatted template
+    @param data: dict
     """
-    t = []
-    for x in data:
-        if type(x) == type({}):
-            t.append(template.format(**x))
-        else:
-            t.append(x)
-    return '\n'.join(t)
-
+    return Template(template).render(data=data)
 
 def multiline_dict( template, data ):
     t = []
