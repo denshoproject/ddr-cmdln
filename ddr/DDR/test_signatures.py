26bacd8b4a29df3b2aa595bde1c2d78326e164d0
geoffrey jost <geoffrey@jostwebwerks.com>
2018-08-30 16:43:57 -0700

    test_signatures: added unit tests
    test_signatures: PART II

---
+++ ddr/DDR/test_signatures.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import git
+import pytest
+
+from . import config
+from . import identifier
+from .models import collection, entity, files
+from . import signatures
+from . import util
+
+TESTING_BASE_DIR = os.path.join(config.TESTING_BASE_DIR, 'signatures')
+if not os.path.exists(TESTING_BASE_DIR):
+    os.makedirs(TESTING_BASE_DIR)
+
+COLLECTION_ID = 'ddr-testing-123'
+ENTITY_IDS = [
+    ('ddr-testing-123-1', True, 'completed'),
+    ('ddr-testing-123-2', True, 'completed'),
+    ('ddr-testing-123-3', True, 'completed'),
+]
+FILE_IDS = [
+    ('ddr-testing-123-1-mezzanine-abc123', True, 'completed'),
+    ('ddr-testing-123-1-master-abc123', True, 'completed'),
+    ('ddr-testing-123-2-mezzanine-abc124', True, 'completed'),
+    ('ddr-testing-123-2-master-abc124', True, 'completed'),
+    ('ddr-testing-123-3-mezzanine-a1b2c3', True, 'completed'),
+    ('ddr-testing-123-3-master-a1b2c3', True, 'completed'),
+]
+
+SIGNATURES = [
+    ('ddr-testing-123', 'ddr-testing-123-1-mezzanine-abc123'),
+    ('ddr-testing-123-1', 'ddr-testing-123-1-mezzanine-abc123'),
+    ('ddr-testing-123-2', 'ddr-testing-123-2-mezzanine-abc124'),
+    ('ddr-testing-123-3', 'ddr-testing-123-3-mezzanine-a1b2c3'),
+]
+
+def test_setup_test_collection():
+    oid = COLLECTION_ID
+    cpath = os.path.join(TESTING_BASE_DIR, oid)
+    repo = git.Repo.init(cpath)
+    oi = identifier.Identifier(id=oid, base_path=TESTING_BASE_DIR)
+    o = collection.Collection.create(oi.path_abs(), oi)
+    o.public = True
+    o.status = 'completed'
+    o.write_json()
+    
+    for oid,public,status in ENTITY_IDS:
+        oi = identifier.Identifier(id=oid, base_path=TESTING_BASE_DIR)
+        o = entity.Entity.create(oi.path_abs(), oi)
+        o.public = public
+        o.status = status
+        o.write_json()
+    
+    for oid,public,status in FILE_IDS:
+        oi = identifier.Identifier(id=oid, base_path=TESTING_BASE_DIR)
+        o = files.File.create(oi.path_abs(), oi)
+        o.public = public
+        o.status = status
+        o.write_json()
+
+def test_pick_signatures():
+    cpath = os.path.join(TESTING_BASE_DIR, COLLECTION_ID)
+    paths = util.find_meta_files(cpath, recursive=True, force_read=True, testing=True)
+    parents = signatures.choose(paths)
+    updates = signatures.find_updates(parents)
+    files_written = signatures.write_updates(updates)
+    return files_written
+
+def test_check_signatures():
+    for oid,sid in SIGNATURES:
+        o = identifier.Identifier(id=oid, base_path=TESTING_BASE_DIR).object()
+        print(oid,sid,o.signature_id)
+        assert sid == o.signature_id
