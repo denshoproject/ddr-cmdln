d22b7ba0d22c4e46391379402b86a7d6094a3261
geoffrey jost <geoffrey@jostwebwerks.com>
2018-08-30 14:07:38 -0700

    test_archivedotorg: added unit tests

--- ddr/DDR/archivedotorg.py
+++ ddr/DDR/archivedotorg.py
@@ -24,6 +24,11 @@ FIELDNAMES = ['sha1','size','length','height','width','title',]
 
 
 def is_iaobject(o):
+    """Determines whether or not to check Internet Archive for this object
+    
+    @param o: Identifier
+    @returns: boolean
+    """
     if (o.identifier.model in IA_HOSTED_MODELS) \
     or (o.format in IA_HOSTED_FORMATS):
         return True
@@ -32,6 +37,7 @@ def is_iaobject(o):
 
 class IAObject():
     id = ''
+    format = ''
     xml_url = ''
     http_status = -1
     original = ''
@@ -56,27 +62,64 @@ class IAObject():
         """
         o = IAObject()
         o.id = oid
-        o.xml_url = _xml_url(oid)
+        o.xml_url = _xml_url(o.id)
         if http_status and xml:
             o.http_status,o.xml = http_status,xml
         else:
-            o.http_status,o.xml = get_xml(o.id)
-        soup = BeautifulSoup(o.xml, 'html.parser')
-        o.original = _get_original(o.id, soup)
+            o.http_status,o.xml = get_xml(oid)
+        o.soup = BeautifulSoup(o.xml, 'html.parser')
+        o.original = o._get_original()
         if not o.original:
             # interview entities don't have any actual files
             return None
-        # files
-        for format_ in FORMATS:
-            for tag in soup.files.children:
-                if isinstance(tag, Tag) and (format_ in tag['name']):
-                    o.files[format_] = _file_meta(o.id, format_, tag)
-        # assign mimetype
-        for format_,f in o.files.iteritems():
-            if f.name == o.original:
-                o.mimetype = f.mimetype
-                break
+        o._gather_files_meta()
+        o._assign_mimetype()
         return o
+    
+    def _get_original(self):
+        """
+        Archive.org objects correspond to the DDR *Entity* or *Segment*, not *File*.
+        Archive.org objects have many files, each marked as "original" or "derivative".
+        Many files marked "original" are not actually original, e.g. *.asr.srt.
+        One file marked "original" *should* be the one uploaded by Densho.
+        Files uploaded by Densho are in a limited set of formats.
+        """
+        files = [
+            tag for tag in self.soup('file', source='original')
+            if os.path.splitext(tag['name'])[1].replace('.','') in FORMATS
+        ]
+        if not files:
+            return None
+        if len(files) > 1:
+            raise Exception('Found multiple original files for %s' % self.id)
+        orig = files[0]
+        return orig['name']
+    
+    def _gather_files_meta(self):
+        for format_ in FORMATS:
+            for tag in self.soup.files.children:
+                if isinstance(tag, Tag) and (format_ in tag['name']):
+                    self.files[format_] = self._file_meta(format_, tag)
+    
+    def _file_meta(self, format_, tag):
+        f = IAFile()
+        f.name = tag['name']
+        f.format = format_
+        f.url = _file_url(self.id, tag)
+        f.mimetype,f.encoding = mimetypes.guess_type(tag['name'])
+        for field in FIELDNAMES:
+            f._fields.append(field)
+            try:
+                setattr(f, field, tag.find(field).contents[0])
+            except AttributeError:
+                setattr(f, field, '')
+        return f
+    
+    def _assign_mimetype(self):
+        for format_,f in self.files.iteritems():
+            if f.name == self.original:
+                self.mimetype = f.mimetype
+                break
     
     def original_file(self):
         base,ext = os.path.splitext(self.original)
@@ -123,14 +166,13 @@ class IAFile():
             data[field] = getattr(self, field)
         return data
 
+    
 def _xml_url(oid):
-    """Given an object id, returns an Internet Archive URL
-    
-    @param oid: str object ID
-    @returns: url str
-    """
-    return SEGMENT_XML_URL.format(base=IA_DOWNLOAD_URL, segmentid=oid)
-    
+    return SEGMENT_XML_URL.format(
+        base=IA_DOWNLOAD_URL,
+        segmentid=oid
+    )
+
 def get_xml(oid):
     """HTTP request for IA metadata, returns HTTP code w data
     
@@ -141,39 +183,6 @@ def get_xml(oid):
     if r.status_code == 200:
         return r.status_code,r.text
     return r.status_code,''
-
-def _get_original(oid,soup):
-    """
-    Archive.org objects correspond to the DDR *Entity* or *Segment*, not *File*.
-    Archive.org objects have many files, each marked as "original" or "derivative".
-    Many files marked "original" are not actually original, e.g. *.asr.srt.
-    One file marked "original" *should* be the one uploaded by Densho.
-    Files uploaded by Densho are in a limited set of formats.
-    """
-    files = [
-        tag for tag in soup('file', source='original')
-        if os.path.splitext(tag['name'])[1].replace('.','') in FORMATS
-    ]
-    if not files:
-        return None
-    if len(files) > 1:
-        raise Exception('Found multiple original files for %s' % oid)
-    orig = files[0]
-    return orig['name']
-
-def _file_meta(oid, format_, tag):
-    f = IAFile()
-    f.name = tag['name']
-    f.format = format_
-    f.url = _file_url(oid, tag)
-    f.mimetype,f.encoding = mimetypes.guess_type(tag['name'])
-    for field in FIELDNAMES:
-        f._fields.append(field)
-        try:
-            setattr(f, field, tag.find(field).contents[0])
-        except AttributeError:
-            setattr(f, field, '')
-    return f
 
 def _file_url(oid, tag):
     return FILE_DOWNLOAD_URL.format(
