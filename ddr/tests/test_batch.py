ec2ae43b5ca3d553320bd2bb9708df2266678b1c
geoffrey jost <geoffrey@jostwebwerks.com>
2019-02-27 12:26:23 -0800

    batch: Added tests for testable functions

--- ddr/tests/test_batch.py
+++ ddr/tests/test_batch.py
@@ -65,6 +65,16 @@ class TestChecker():
         expected3 = ('entity',[])
         out3 = batch.Checker._guess_model(rowds3)
         assert out3 == expected3
+        # segments
+        rowds3 = [
+            {
+                'id':'ddr-testing-123-4-5',
+                'identifier': identifier.Identifier('ddr-testing-123-4-5'),
+            },
+        ]
+        expected3 = ('segment',[])
+        out3 = batch.Checker._guess_model(rowds3)
+        assert out3 == expected3
         # files
         rowds4 = [
             {
@@ -121,19 +131,101 @@ class TestChecker():
 
 
 class TestImporter():
-    pass
-    # TODO def test_fidentifier_parent(self):
+    
+    def test_fidentifier_parent(self):
+        ei = identifier.Identifier('ddr-testing-123-4')
+        si = identifier.Identifier('ddr-testing-123-4-5')
+        efi = identifier.Identifier('ddr-testing-123-4-master-abc123')
+        sfi = identifier.Identifier('ddr-testing-123-4-5-master-abc123')
+        out0 = batch.Importer._fidentifier_parent(efi)
+        out1 = batch.Importer._fidentifier_parent(sfi)
+        assert out0.id == ei.id
+        assert out1.id == si.id
+        
     # TODO def test_write_entity_changelog(self):
     # TODO def test_write_file_changelogs(self):
     # TODO def test_import_entities(self):
     # TODO def test_csv_load(self):
-    # TODO def test_fidentifiers(self):
-    # TODO def test_fid_parents(self):
+
+    def test_fidentifiers(self):
+        ci = identifier.Identifier('ddr-testing-123')
+        rowds0 = [
+            {'id': 'ddr-testing-123'},
+            {'id': 'ddr-testing-123-4'},
+            {'id': 'ddr-testing-123-4-master'},
+            {'id': 'ddr-testing-123-4-master-abc123'},
+            {'id': 'ddr-testing-123-4-5'},
+            {'id': 'ddr-testing-123-4-5-master'},
+            {'id': 'ddr-testing-123-4-5-master-abc123'},
+        ]
+        out0 = batch.Importer._fidentifiers(rowds0, ci)
+        print(out0)
+        print(out0.keys())
+        expected_keys = [
+            'ddr-testing-123-4-master-abc123', 'ddr-testing-123-4-5-master-abc123',
+        ]
+        assert out0.keys() == expected_keys
+    
+    def test_fid_parents(self):
+        ci = identifier.Identifier('ddr-testing-123')
+        
+        # new files
+        fids0 = {}
+        rowds0 = [
+            {'id': 'ddr-testing-123-4'},
+            {'id': 'ddr-testing-123-4-master-abc123'},
+            {'id': 'ddr-testing-123-4-5'},
+            {'id': 'ddr-testing-123-4-5-master-abc123'},
+        ]
+        out0 = batch.Importer._fid_parents(fids0, rowds0, ci)
+        expected0 = [
+            'ddr-testing-123-4',
+            'ddr-testing-123-4-5',
+        ]
+        assert out0.keys() == expected0
+        
+        # existing files
+        fid10 = identifier.Identifier('ddr-testing-123-4-master-abc123')
+        fid11 = identifier.Identifier('ddr-testing-123-4-5-master-abc123')
+        fids1 = {
+            'ddr-testing-123-4-master-abc123': fid10,
+            'ddr-testing-123-4-5-master-abc123': fid11,
+        }
+        rowds1 = [
+            {'id': 'ddr-testing-123-4-master-abc123'},
+            {'id': 'ddr-testing-123-4-5-master-abc123'},
+        ]
+        out1 = batch.Importer._fid_parents(fids1, rowds1, ci)
+        expected1 = [
+            'ddr-testing-123-4-master-abc123',
+            'ddr-testing-123-4-5-master-abc123',
+        ]
+        assert out1.keys() == expected1
+    
     # TODO def test_eidentifiers(self):
     # TODO def test_existing_bad_entities(self):
     # TODO def test_file_objects(self):
     # TODO def test_rowds_new_existing(self):
-    # TODO def test_rowd_is_external(self):
+    
+    def test_rowd_is_external(self):
+        rowds = [
+            {'external': 0},
+            {'external': '0'},
+            {'external': 1},
+            {'external': '1'},
+        ]
+        expected = [
+            False,
+            False,
+            True,
+            True
+        ]
+        out = [
+            batch.Importer._rowd_is_external(rowd)
+            for rowd in rowds
+        ]
+        assert out == expected
+    
     # TODO def test_import_files(self):
     # TODO def test_update_existing_files(self):
     # TODO def test_add_new_files(self):
