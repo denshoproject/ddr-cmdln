15042332e4d9ea4fc3c60643210e70574832db76
Geoffrey Jost <geoffrey.jost@densho.org>
2024-10-15 09:30:18 -0700

    ddrremote: Remove check and help subcommands
    ddrremote copy does the check from the check subcommand when it
    decides whether to copy files, so the check subcommand is
    redundant.  With that there is now only the copy subcommand so
    the help subcommand is redundant.
    
    This command was a wrapper around `git annex checkpresentkey` which
    queried the remote for the presence of a file.
    `git annex copy` runs this command before copying a file (or not,
    if it's already there) so ddrremote check was an interesting
    exercise but is redundant.

--- ddr/DDR/cli/ddrremote.py
+++ ddr/DDR/cli/ddrremote.py
@@ -1,16 +1,5 @@
 DESCRIPTION = """ddrremote - Tools for working with DDR git-annex special remotes"""
 
-HELP = """Tools for working with DDR git-annex special remotes.
-
-check - Verifies that all the annex files in the collection are present in
-        the specified special remote. Optionally writes to --log.
-copy  - Runs git annex copy to the specified special remote, and
-        optionally writes to --log.
-recap - Reports info about recent collection modifications.
-
-- Works best if the ddr user has passwordless SSH keys for each remote.
-"""
-
 
 from datetime import datetime
 from zoneinfo import ZoneInfo
@@ -48,139 +37,9 @@ def log(logfile, line):
 @click.option('--debug','-d', is_flag=True, default=False)
 def ddrremote(debug):
     """ddrremote - Tools for working with DDR git-annex special remotes
-
-    \b
-    See "ddrremote help" for examples.
     """
     if debug:
         click.echo('Debug mode is on')
-
-
-@ddrremote.command()
-def help():
-    """Detailed help and usage examples
-    """
-    click.echo(HELP)
-
-
-@ddrremote.command()
-@click.option('-f','--fast', is_flag=True, default=False, help='Faster check using git annex info.')
-@click.option('-l','--logdir', default=None, help='Write output to log file in directory.')
-@click.option('-v','--verbose', is_flag=True, default=False, help='Show all files not just missing ones.')
-@click.option('-w','--wait', default=0, help='Wait N seconds after checking a collection.')
-@click.argument('remote')
-@click.argument('collection')
-def check(fast, logdir, verbose, wait, remote, collection):
-    """Check that annex files in the collection are present in the special remote
-    
-    Under the hood, uses the git annex checkpresentkey plumbing command,
-    which reaches out to the special remote.
-    
-    Write output to files for use by compare command:
-    ddrremote check REMOTE COLLECTIONPATH | tee /PATH/REMOTE/COLLECTION
-    """
-    collection_path = Path(collection).absolute()
-    cid = collection_path.name
-    prefix = f"{dtfmt()} ddrremote check"
-    logfile = Path(logdir) / f"{cid}.log" if logdir else None
-    starttime = datetime.now(tz=config.TZ)
-    log(logfile, f"{dtfmt()} ddrremote check {remote} {collection_path} START")
-    if fast:
-        size_here,size_remote,diff = annex_info_remote(collection_path, remote)
-        endtime = datetime.now(tz=config.TZ); elapsed = endtime - starttime
-        def natural(filesize):
-            if filesize == None: return '---'
-            if filesize == 0: return '0'
-            return humanize.naturalsize(filesize).replace(' ','') if filesize else '---'
-        here_nat = natural(size_here)
-        remote_nat = natural(size_remote)
-        diff_nat = natural(diff)
-        log(logfile, f"{dtfmt()} ddrremote check {remote} {collection_path} DONE {elapsed} {here_nat} here {remote_nat} remote {diff_nat} missing")
-    else:
-        startdir = os.getcwd()
-        annex_files = annex_find(collection_path)
-        errors = 0
-        missing = 0
-        for rel_path in annex_files:
-            output = checkpresentkey(collection_path, rel_path, remote)
-            if output.split()[0] == 'ok':
-                if (verbose):
-                    log(logfile, f"{prefix} {output}")
-            else:
-                errors += 1
-                if 'missing' in output:
-                    missing += 1
-                log(logfile, f"{prefix} {output}")
-        endtime = datetime.now(tz=config.TZ); elapsed = endtime - starttime
-        log(logfile, f"{dtfmt()} ddrremote check {remote} {collection_path} DONE {str(elapsed)} {len(annex_files)} files {errors} errs {missing} missing")
-    if wait:
-        sleep(int(wait))
-
-def annex_find(collection_path):
-    """Gets list of relative file paths using git annex find
-    """
-    os.chdir(collection_path)
-    return [
-        Path(relpath)
-        for relpath in subprocess.check_output(
-                'git annex find --include "*"', stderr=subprocess.STDOUT, shell=True,
-                encoding='utf-8'
-        ).strip().splitlines()
-    ]
-
-def checkpresentkey(collection_path, rel_path, remote):
-    # get file's annex filename AKA key
-    os.chdir(collection_path)
-    key = None
-    rpath = str(rel_path.resolve())
-    if rel_path.is_symlink() and '.git/annex/objects' in rpath and 'SHA' in rpath:
-        key = rel_path.resolve().name
-    if not key:
-        return f"ERROR nokey {str(rel_path)}"
-    # git annex checkpresentkey
-    cmd = f"git annex checkpresentkey {key} {remote}"
-    try:
-        # checkpresentkey returns 0 if file is present
-        output = subprocess.check_output(
-            cmd, stderr=subprocess.STDOUT, shell=True,
-            encoding='utf-8'
-        )
-        if output == '':
-            return f"ok {str(rel_path)}"
-    except subprocess.CalledProcessError as err:
-        # checkpresentkey returns 1 if file is *absent*
-        if err.returncode == 1:
-            return f"ERROR missing {remote} {str(rel_path)}"
-        # checkpresentkey returns 100 if something went wrong
-        return f"ERROR {err}"
-    return f"ERROR fellout {str(rel_path)}"
-
-def annex_info_remote(collection_path, remote):
-    """Get size diff between here and specified remote
-    
-    Uses `git annex info .`
-    """
-    os.chdir(collection_path)
-    cmd = 'git annex info . --bytes --json'
-    data = json.loads(subprocess.check_output(
-        cmd, stderr=subprocess.STDOUT, shell=True,
-        encoding='utf-8'
-    ))
-    here = None
-    for r in data['repositories containing these files']:
-        if r['here']:
-            r['size'] = int(r['size'])
-            here = r; continue
-    there = None
-    for r in data['repositories containing these files']:
-        if remote in r['description']:
-            r['size'] = int(r['size'])
-            there = r; continue
-    if there:
-        return here['size'], there['size'], here['size'] - there['size']
-    if here:
-        return here['size'],None,None
-    return None,None,None
 
 
 @ddrremote.command()
@@ -260,10 +119,11 @@ def copy(logdir, jobs, backoff, wait, remote, collection):
     starttime = datetime.now(tz=config.TZ)
     os.chdir(collection_path)
     log(logfile, f"{dtfmt()} ddrremote copy {remote} {collection_path} START")
-    files = len(annex_find(collection_path))
+    annex_files = _annex_find(collection_path)
+    files = len(annex_files)
     ok = 0; copied = 0; errors = 0
     if backoff:
-        for relpath in annex_find(collection):
+        for relpath in annex_files:
             ok,copied,errors = _analyze_annex_copy_output(
                 _annex_copy_file(relpath, remote),
                 remote, ok, copied, errors, prefix, logfile
@@ -281,6 +141,18 @@ def copy(logdir, jobs, backoff, wait, remote, collection):
     log(logfile, f"{operation} DONE {elapsed} {status}")
     if wait:
         sleep(float(wait))
+
+def _annex_find(collection_path):
+    """Gets list of relative file paths using git annex find
+    """
+    os.chdir(collection_path)
+    return [
+        Path(relpath)
+        for relpath in subprocess.check_output(
+                'git annex find --include "*"', stderr=subprocess.STDOUT, shell=True,
+                encoding='utf-8'
+        ).strip().splitlines()
+    ]
 
 def _annex_copy_all(collection_path, remote, jobs=''):
     """git annex copy . and return output or error
