e62294b46a0386288ced39d3b381654a305380d4
Geoffrey Jost <geoffrey.jost@densho.org>
2025-04-22 11:49:17 -0700

    bin: Update chown syntax in fpm-after-install

--- bin/fpm-after-install.sh
+++ bin/fpm-after-install.sh
@@ -1,13 +1,13 @@
 # Add symlinks after installing (FPM) .deb package
 
 touch /etc/ddr/ddrlocal-local.cfg
-chown ddr.root /etc/ddr/ddrlocal-local.cfg
+chown ddr:root /etc/ddr/ddrlocal-local.cfg
 chmod 640 /etc/ddr/ddrlocal-local.cfg
 
 # logs dir perms
 mkdir -p /var/log/ddr
 chmod 755 /var/log/ddr
-chown -R ddr.ddr /var/log/ddr
+chown -R ddr:ddr /var/log/ddr
 
 # Install customized ImageMagick-6/policy.xml.  This disables default
 # memory and cache limits put in place to protect against DDoS attacks
